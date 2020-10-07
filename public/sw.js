c84ee3411e9798846604c4ef33d53d48308d8654
apurvatrpth <at131296@gmail.com>
2020-10-07 15:59:37 +0530

    Add service working for caching pages

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+let cacheData = 'app-cache-apurv';
+
+this.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        '/static/js/bundle.js',
+        '/static/js/1.chunk.js',
+        '/static/js/main.chunk.js',
+        'main.f22661d62f47859372c0.hot-update.js',
+        '/index.html',
+        '/',
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      if (res) {
+        return res;
+      }
+    })
+  );
+});
---
+++ src/swDev.js
@@ -0,0 +1,8 @@
+const swDev = () => {
+  let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+  navigator.serviceWorker.register(swUrl).then((response) => {
+    console.warn('res:', response);
+  });
+};
+
+export default swDev;
